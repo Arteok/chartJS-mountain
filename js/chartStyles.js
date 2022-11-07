1e3eebfbf328ddef8c02e26ba13f3dd090ab49d5
Pablo Mariasch <103390999+Arteok@users.noreply.github.com>
2022-11-07 03:15:19 -0300

    06-11-2022

--- js/chartStyles.js
+++ js/chartStyles.js
@@ -1,6 +1,11 @@
-const style = {
+const style = {//no anda, por eso uso el de abajo que saque de otro video    
     color: {
-        solids:['rgba(116, 72, 194, 1)','rgba(33, 192, 215, 1)','rgba(217, 158, 43, 1)','rgba(205, 58, 129, 1)'],
-        alphas:['rgba(116, 72, 194, 1)','rgba(33, 192, 215, 1)','rgba(217, 158, 43, 1)','rgba(205, 58, 129, 1)']
+        solids:['rgba(116, 72, 194, 1)','rgba(33, 192, 215, 1)','rgba(217, 158, 43, 1)','rgba(205, 58, 129, 1)','rgba(33, 192, 215, 1)','rgba(217, 158, 43, 1)','rgba(205, 58, 129, 1)','rgba(156, 153, 204, 1)','rgba(225, 78, 202, 1)'],
+        alphas:['rgba(116, 72, 194, 2)','rgba(33, 192, 215, 2)','rgba(217, 158, 43, 2)','rgba(205, 58, 129, 2)','rgba(33, 192, 215, 2)','rgba(217, 158, 43, 2)','rgba(205, 58, 129, 2)','rgba(156, 153, 204, 2)','rgba(225, 78, 202, 2)']
     }
+}
+
+const getDataColors = opacity => {
+    const colors = ['#7448c2','#21c0d7','#d99e2b','#cd3a81','#9c99cc','#e14eca','#ffffff','#ff0000','#d6ff00','#000000']
+    return colors.map(color => opacity ? `${color + opacity}` : color)
 }
--- js/script.js
+++ js/script.js
@@ -1,9 +1,52 @@
+Chart.defaults.color = '#fff'
+
 /*trae el json de la pagina web */
 fetch('https://coasters-api.herokuapp.com/')
 .then(response => response.json())
 .then(data => printCharts(data))
 
 function printCharts(coasters) {
-    document.body.classList.add('running')
+    document.body.classList.add('running');//desaparece "esperando la respuesta de la API" creando una class running en el body
+    compareRadialChart(coasters, 'chart2')
     
+}
+
+function compareRadialChart(coasters, id) {
+    const data = {
+        labels: ['EEUU','China','Japón','España','Reino Unido'],
+        datasets: [{
+            data: [
+                coasters.filter(eachCoaster => eachCoaster.country === 'United States').length,
+                coasters.filter(eachCoaster => eachCoaster.country === 'China').length,
+                coasters.filter(eachCoaster => eachCoaster.country === 'Japan').length,
+                coasters.filter(eachCoaster => eachCoaster.country === 'Spain').length,
+                coasters.filter(eachCoaster => eachCoaster.country === 'United Kingdom').length
+            ],
+            bordeWidth: 1,
+            borderColor: getDataColors(),
+            backgroundColor: getDataColors(40)                       
+        }]
+    }
+
+    const options = {
+        
+        scales: {
+            r: {
+                grid: {
+                    color: '#7f8c8d',
+
+                },
+                ticks: {
+                    display: false
+                }
+            }
+        },
+
+        plugins: {
+            legend: { position: 'right'},   
+                
+        }
+    }
+
+    new Chart('chart2',{type: 'polarArea', data, options});
 }
